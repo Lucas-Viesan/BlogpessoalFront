63ebb885fd385b7239976a20d84741b3ee6d8e7f
Lucas-Viesan <vieiratm01@hotmail.com>
2025-02-17 11:05:20 -0300

    Implementado CRUD frontend

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -19,3 +19,40 @@ export const login = async (url: string, dados: object, setDados: Function) => {
   const resposta = await api.post(url, dados);
   setDados(resposta.data);
 };
+
+export const buscar = async (
+  url: string,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  setDados: Function,
+  // eslint-disable-next-line @typescript-eslint/no-wrapper-object-types
+  header: Object
+) => {
+  const resposta = await api.get(url, header);
+  setDados(resposta.data);
+};
+
+export const cadastrar = async (
+  url: string,
+  dados: object,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  setDados: Function,
+  header: object
+) => {
+  const resposta = await api.post(url, dados, header);
+  setDados(resposta.data);
+};
+
+export const atualizar = async (
+  url: string,
+  dados: object,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  setDados: Function,
+  header: object
+) => {
+  const resposta = await api.put(url, dados, header);
+  setDados(resposta.data);
+};
+
+export const deletar = async (url: string, header: object) => {
+  await api.delete(url, header);
+};
